915789fe9185f446a976c88934b04e77749a41cb
sujin-create <qsoojinq@naver.com>
2022-07-23 14:40:39 +0900

    ADD : Account app 추가

---
+++ finance_help/Account/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+    
+class User(AbstractUser):
+    username = models.CharField(max_length=100, unique = True)
+    email = models.EmailField(null=False, unique = True)
+    password = models.CharField(max_length=20)
+
+    USERNAME_FIELD =  'email'
+    REQUIRED_FIELDS =  []
